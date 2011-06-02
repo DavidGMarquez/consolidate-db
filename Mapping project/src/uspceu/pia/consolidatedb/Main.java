75beb9fb34381c7bac795cb096216d0d223e3d14
Sir.dg.mv <DavidGMarquez@users.noreply.github.com>
2011-06-02 08:55:37 +0000

    Version final, algunos cambios mínimos y algunas cosas inútiles borradas

--- Mapping project/src/uspceu/pia/consolidatedb/Main.java
+++ Mapping project/src/uspceu/pia/consolidatedb/Main.java
@@ -2,64 +2,17 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package uspceu.pia.consolidatedb;
 
-class Main
-{
-    public static void main(String[] args)
-    {
-       
+class Main {
+
+    public static void main(String[] args) {
         GeneradorEntidades entidades1 = new GeneradorEntidades("piadb", "pia", "pia");
         GeneradorEntidades entidades2 = new GeneradorEntidades("piaDbaux", "pia", "pia");
-        GeneradorMapeados mapeado=new GeneradorMapeados(entidades1.getEntidades(),entidades2.getEntidades());
-
-        //con esta linea vemos los atributos finales de cada entidad
-
-                //mapeado.entidadesFinales.escribirlistaConAtributos();
-        
-        // Con esta linea podemos ver el mapeo de atributos
-
-                //mapeado.entidadesFinales.escribirlistaConAtributosFinales();
-
-        GestorDeConsultas gestorDeConsultas=new GestorDeConsultas(entidades1.getEntidades(),entidades2.getEntidades(),mapeado.getEntidadesFinales());
+        GeneradorMapeados mapeado = new GeneradorMapeados(entidades1.getEntidades(), entidades2.getEntidades());
+        System.out.println("---------");
+        mapeado.entidadesFinales.escribirlistaConAtributos();
+        GestorDeConsultas gestorDeConsultas = new GestorDeConsultas(entidades1.getEntidades(), entidades2.getEntidades(), mapeado.getEntidadesFinales());
         gestorDeConsultas.realizarConsulta();
-        //Obtener resultado global
-        
     }
-
-    /*
-     * Consultas
-     *
-     Obtener resultados con un join
-     select * from piadb.personas natural join piadbaux.personas
-     *
-     Obtener resultados que difieren (que no estan en las dos tablas)
-     select * from piadb.personas left outer join piadbaux.personas on piadbaux.personas.idpersonas=piadb.personas.idpersonas
-where piadbaux.personas.idpersonas is null
-union all
-select * from piadb.personas right outer join piadbaux.personas on piadbaux.personas.idpersonas=piadb.personas.idpersonas
-where piadb.personas.idpersonas is null;
-     *
-     Obtener todo junto
-     select * from piadb.personas left outer join piadbaux.personas on piadbaux.personas.idpersonas=piadb.personas.idpersonas
-union all
-select * from piadb.personas right outer join piadbaux.personas on piadbaux.personas.idpersonas=piadb.personas.idpersonas
-where piadb.personas.idpersonas is null;
-     Como elegir un conjunto de atributos
-     *
-     select piadb.personas.nombre as name,piadb.personas.email,piadb.personas.idpersonas as emailo
-,piadbaux.personas.apellido as apellido
-from piadb.personas
-left outer join piadbaux.personas on piadbaux.personas.idpersonas=piadb.personas.idpersonas
-union all
-select piadb.personas.nombre as name,piadb.personas.email,piadb.personas.idpersonas as emailo
-,piadbaux.personas.apellido as apellido
-from piadb.personas
-right outer join piadbaux.personas on piadbaux.personas.idpersonas=piadb.personas.idpersonas
-where piadb.personas.idpersonas is null;
-     *
-     *
-     *
-     */
 }
